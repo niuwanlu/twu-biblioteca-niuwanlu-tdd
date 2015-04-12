a58574298d294e0b729668cb26f6fa1eaba0412e
niuwanlu <niuwanlu@gmail.com>
2015-04-12 17:36:00 +0800

    Story2 : List Books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -2,7 +2,27 @@ package com.twu.biblioteca;
 
 public class BibliotecaApp {
 
+    private String[] books;
+
+    public void setBooks(String[] books) {
+        this.books = books;
+    }
+
+    public String[] getBooks() {
+        return books;
+    }
+
     public static void main(String[] args) {
         System.out.println("Welcome!");
+        System.out.println("=======================");
     }
+
+    public void printBookList() {
+        String[] books = getBooks();
+        System.out.println("=======Book List=======");
+        for (String book : books ) {
+            System.out.println(book);
+        }
+    }
+
 }
--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -5,7 +5,6 @@ import org.junit.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
-
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
@@ -23,9 +22,25 @@ public class ExampleTest {
     }
 
     @Test
-    public void out() {
+    public void testWelcomeMessage() {
         BibliotecaApp.main(new String[] {});
-        assertEquals("Welcome!\n", outContent.toString());
+        String output = outContent.toString();
+        String[] linesOfOutput = output.split(System.getProperty("line.separator"));
+        assertEquals("Welcome!", linesOfOutput[0]);
+        assertEquals("=======================", linesOfOutput[1]);
+    }
+
+    @Test
+    public void testBookList() {
+        BibliotecaApp biblioteca = new BibliotecaApp();
+        String[] bookList = {"Harry Potter", "Head First Java"};
+        biblioteca.setBooks(bookList);
+        biblioteca.printBookList();
+        String output = outContent.toString();
+        String[] linesOfOutput = output.split(System.getProperty("line.separator"));
+        assertEquals("=======Book List=======", linesOfOutput[0]);
+        assertEquals(bookList[0], linesOfOutput[1]);
+        assertEquals(bookList[1], linesOfOutput[2]);
     }
 
 }
