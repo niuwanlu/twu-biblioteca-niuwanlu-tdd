a1fb67ea47399cf0741b44b82f5086e3057ec449
niuwanlu <niuwanlu@gmail.com>
2015-04-12 16:50:38 +0800

    Story1: Welcome Message

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,31 @@
 package com.twu.biblioteca;
 
 
-import org.junit.Test;
+import org.junit.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
-    @Test
-    public void test() {
-        assertEquals(1, 1);
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
     }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
+    @Test
+    public void out() {
+        BibliotecaApp.main(new String[] {});
+        assertEquals("Welcome!\n", outContent.toString());
+    }
+
 }
