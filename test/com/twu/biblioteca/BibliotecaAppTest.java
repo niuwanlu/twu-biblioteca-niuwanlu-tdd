a3316a97e4bb86f26a13f796e91fd7ec37d2de66
niuwanlu <niuwanlu@gmail.com>
2015-04-17 22:54:34 +0800

    Story 1 : welcome message Refactoring with spy

--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -6,6 +6,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import static org.mockito.Mockito.*;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
@@ -15,24 +16,11 @@ import static org.junit.Assert.assertEquals;
 
 public class BibliotecaAppTest {
 
-    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
-
-    @Before
-    public void setUpStreams() {
-        System.setOut(new PrintStream(outContent));
-    }
-
-    @After
-    public void cleanUpStreams() {
-        System.setOut(null);
-    }
-
     @Test
     public void testShowWelcome() {
-        BibliotecaApp app = new BibliotecaApp();
+        BibliotecaApp app = Mockito.spy(new BibliotecaApp());
         app.start();
-        String output = outContent.toString();
-        assertEquals("Welcome to Bivlioteca!\n", output);
+        verify(app, times(1)).showWelcome();
     }
 
 
